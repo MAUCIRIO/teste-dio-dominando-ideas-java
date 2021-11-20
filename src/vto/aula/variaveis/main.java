5e6628a55a11f1858c09309fc1b0328cac6b9a4c
Maucirio <maucirioprof@gmail.com>
2021-11-20 11:32:37 -0300

    second commit

---
+++ src/vto/aula/variaveis/main.java
@@ -0,0 +1,49 @@
+package vto.aula.variaveis;
+
+public class main {
+    public static void main(String[] args) {
+
+        int i = 5;
+        int I = 10;
+        int a = 5;
+        int b = 7;
+        int $aq = 0;
+
+        final int j = 10;
+        int asrn24678md;
+        int asrn2$4678_md = 10;
+
+        asrn24678md = 100;
+        asrn2$4678_md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        int QUANTIDADE_OPCOES = 25;
+        int qtdProd;
+        System.out.println(i);
+        System.out.println(I);
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        System.out.println(asrn2$4678_md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
